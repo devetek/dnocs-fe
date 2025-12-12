7dedfb252918cc098f0864bc2958f8ddc6fa07eb
Prakasa <prakasa@devetek.com>
2025-12-12 14:16:41 +0700

    fix: set static env variable related domain

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export VITE_ENVIRONMENT="beta"
+export VITE_FRONTEND="https://dnocs-v3.beta.devetek.app"
+export VITE_BACKEND="https://pawon-beta.terpusat.com"
+
 node ./.output/server/index.mjs
