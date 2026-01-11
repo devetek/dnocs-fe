1ce677c99be54b07cdd23eff3bc2243f6955e772
Aldey Wahyu Putra <al.arudei01@gmail.com>
2026-01-11 16:05:43 +0700

    feat: Array.prototype.toTuple

---
+++ src/shared/libs/browser/array.ts
@@ -0,0 +1,23 @@
+export type Tuple<
+  T,
+  N extends number,
+  R extends T[] = [],
+> = R['length'] extends N ? R : Tuple<T, N, [T, ...R]>;
+
+declare global {
+  interface Array<T> {
+    toTuple<N extends number>(length: N): Tuple<T, N>;
+  }
+}
+
+Array.prototype.toTuple = function <T, N extends number>(
+  length: N,
+): Tuple<T, N> {
+  if (this.length < length) {
+    throw new Error(
+      `Expected array of length >= ${length}, but got ${this.length}`,
+    );
+  }
+
+  return this as Tuple<T, N>;
+};
