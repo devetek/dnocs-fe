4e1c1aedab19737b8478a30187f036c84d82f095
Aldey Wahyu Putra <al.arudei01@gmail.com>
2026-03-08 02:24:15 +0700

    feat: new ButtonV2 to replace Button

--- src/shared/presentation/atoms/Button/Button.tsx
+++ src/shared/presentation/atoms/Button/Button.tsx
@@ -46,6 +46,7 @@ export interface ButtonProps
   asChild?: boolean;
 }
 
+/** @deprecated use Button from `@/shared/presentation/atoms/ButtonV2` instead! */
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
