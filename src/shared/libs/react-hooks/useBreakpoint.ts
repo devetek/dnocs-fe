96942c6d473bcdf0dd3df8a4d932bda8c94447d4
Aldey Wahyu Putra <al.arudei01@gmail.com>
2026-03-08 09:09:28 +0700

    feat(useBreakpoint): added useBreakpointValues

--- src/shared/libs/react-hooks/useBreakpoint.ts
+++ src/shared/libs/react-hooks/useBreakpoint.ts
@@ -42,3 +42,61 @@ export function useBreakpoint(
 
   return match;
 }
+
+type BreakpointKey = keyof typeof CONFIG;
+
+export function useBreakpointValues() {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState<
+    BreakpointKey | 'xs'
+  >();
+
+  useIsomorphicEffect(() => {
+    const style = window.getComputedStyle(document.documentElement);
+
+    // Convert CSS variables to raw numbers (assuming rem)
+    const getVal = (v: string) => {
+      const result = parseInt(style.getPropertyValue(v), 10);
+
+      return (
+        result * parseFloat(getComputedStyle(document.documentElement).fontSize)
+      );
+    };
+
+    const specs = [
+      { key: 'xs', query: `(max-width: ${getVal(CONFIG.sm) - 0.1}px)` },
+      {
+        key: 'sm',
+        query: `(min-width: ${getVal(CONFIG.sm)}px) and (max-width: ${getVal(CONFIG.md) - 0.1}px)`,
+      },
+      {
+        key: 'md',
+        query: `(min-width: ${getVal(CONFIG.md)}px) and (max-width: ${getVal(CONFIG.lg) - 0.1}px)`,
+      },
+      {
+        key: 'lg',
+        query: `(min-width: ${getVal(CONFIG.lg)}px) and (max-width: ${getVal(CONFIG.xl) - 0.1}px)`,
+      },
+      { key: 'xl', query: `(min-width: ${getVal(CONFIG.xl)}px)` },
+    ];
+
+    const mqls = specs.map((spec) => ({
+      key: spec.key,
+      mql: window.matchMedia(spec.query),
+    }));
+
+    const update = () => {
+      const active = mqls.find((m) => m.mql.matches);
+
+      setCurrentBreakpoint(active?.key as BreakpointKey | 'xs');
+    };
+
+    // Initialize and Listen
+    update();
+    mqls.forEach((m) => m.mql.addEventListener('change', update));
+
+    return () =>
+      mqls.forEach((m) => m.mql.removeEventListener('change', update));
+  }, []);
+
+  return [currentBreakpoint] as const;
+}
