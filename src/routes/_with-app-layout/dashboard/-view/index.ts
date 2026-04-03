a8a80f3e0dc2640091ac5be971eac64768eca7e6
Prakasa <prakasa@devetek.com>
2026-04-03 19:27:03 +0700

    fix: enhance dashboard for better UI/UX

--- src/routes/_with-app-layout/dashboard/-view/index.ts
+++ src/routes/_with-app-layout/dashboard/-view/index.ts
@@ -1,3 +1,4 @@
 export { default as SectionApp } from './SectionApp/SectionApp';
 export { default as SectionMachine } from './SectionMachine/SectionMachine';
 export { default as QuickStatsView } from './QuickStats/QuickStatsView';
+export { default as DashboardView } from './DashboardView';
