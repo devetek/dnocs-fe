49a9f5c565b65fd3471a6f6ce34cec4e8342eaeb
Prakasa <prakasa@devetek.com>
2026-04-09 06:36:30 +0700

    fix: dynamic build and run by env

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-export VITE_ENVIRONMENT="beta"
-export VITE_FRONTEND="https://dnocs-v3.beta.devetek.app"
-export VITE_BACKEND="https://pawon-beta.terpusat.com"
-export VERSION=$(git rev-parse --short HEAD)
-
 node ./.build-${VERSION}/server/index.mjs
