71b24f84b97b02496433dfe2fecb23752e47e8f9
Aldey Wahyu Putra <al.arudei01@gmail.com>
2026-05-17 17:00:43 +0700

    feat: FreestandingAccordion support for first init skip animation

--- src/shared/presentation/atoms/FreestandingAccordion/index.tsx
+++ src/shared/presentation/atoms/FreestandingAccordion/index.tsx
@@ -1,10 +1,12 @@
 import type { HTMLAttributes, ReactNode } from 'react';
 import { useRef, useState } from 'react';
 
+import { f } from '@/shared/libs/browser/fn';
 import { useIsomorphicEffect } from '@/shared/libs/react-hooks/useIsomorphicEffect';
 import { cn } from '@/shared/libs/tailwind/cn';
 
-export interface FreestandingAccordionProps extends HTMLAttributes<HTMLDivElement> {
+export interface FreestandingAccordionProps
+  extends HTMLAttributes<HTMLDivElement> {
   isOpen: boolean;
   children: ReactNode;
   durationMs?: number;
@@ -13,21 +15,30 @@ export interface FreestandingAccordionProps extends HTMLAttributes<HTMLDivElemen
 export function FreestandingAccordion(props: FreestandingAccordionProps) {
   const { isOpen, children, durationMs = 300, className, ...rest } = props;
 
+  const [enableAnimation, setEnableAnimation] = useState(false);
+
   const [shouldRender, setShouldRender] = useState(isOpen);
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [height, setHeight] = useState<number | string>(0);
+  const [isAnimating, setIsAnimating] = useState<boolean | undefined>();
+  const [height, setHeight] = useState<number | undefined>();
   const refContent = useRef<HTMLDivElement>(null);
+
+  if (height != null && !enableAnimation && isAnimating != null) {
+    setEnableAnimation(true);
+  }
 
   // Handle the Mounting / Unmounting Logic
   useIsomorphicEffect(() => {
-    let frame: ReturnType<typeof requestAnimationFrame>;
+    let frame: ReturnType<typeof requestAnimationFrame> | undefined;
     let timeout: ReturnType<typeof setTimeout>;
 
     if (isOpen) {
-      setTimeout(() => setShouldRender(true), 0);
+      requestAnimationFrame(() => {
+        setShouldRender(true);
 
-      // Small delay to ensure the DOM is painted before we measure height
-      frame = requestAnimationFrame(() => setIsAnimating(true));
+        requestAnimationFrame(() => {
+          setIsAnimating(true);
+        });
+      });
     } else {
       setTimeout(() => setIsAnimating(false), 0);
       // Wait for the CSS transition to finish before unmounting
@@ -36,7 +47,9 @@ export function FreestandingAccordion(props: FreestandingAccordionProps) {
 
     return () => {
       clearTimeout(timeout);
-      cancelAnimationFrame(frame);
+      if (frame) {
+        cancelAnimationFrame(frame);
+      }
     };
   }, [isOpen, durationMs]);
 
@@ -46,7 +59,7 @@ export function FreestandingAccordion(props: FreestandingAccordionProps) {
 
     const element = refContent.current;
     const updateHeight = () => {
-      setHeight(isAnimating ? element.scrollHeight : 0);
+      setHeight(element.scrollHeight);
     };
 
     const resizeObserver = new ResizeObserver(updateHeight);
@@ -60,25 +73,35 @@ export function FreestandingAccordion(props: FreestandingAccordionProps) {
   if (!shouldRender) return null;
 
   const cnWrapper = cn(
-    'overflow-hidden transition-[height] ease-in-out',
+    'overflow-hidden',
+    'transition-[height] ease-in-out',
     className,
   );
   const cnContent = cn(
-    'opacity-0 transition-opacity ease-in-out',
-    isAnimating && 'opacity-100',
+    enableAnimation &&
+      cn(
+        'opacity-0 transition-opacity ease-in-out',
+        isAnimating && 'opacity-100',
+      ),
   );
+
+  const finalHeight = f(() => {
+    if (!enableAnimation || height == null) return undefined;
+
+    if (!isAnimating) return '0px';
+
+    return typeof height === 'number' ? `${height}px` : height;
+  });
 
   return (
     <div
       {...rest}
       className={cnWrapper}
-      data-open={isOpen}
       style={{
-        height: typeof height === 'number' ? `${height}px` : height,
+        height: finalHeight,
         transitionDuration: `${durationMs}ms`,
         ...rest.style,
       }}
-      aria-hidden={!isOpen}
     >
       <div
         ref={refContent}
